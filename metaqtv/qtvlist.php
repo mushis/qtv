cb93d1db51c0c54b54a16c2664f8ea2d3fd566e3
Jan Raszyk <jan.raszyk@gmail.com>
2008-07-21 12:10:36 +0000

    added mli's quakeservers' qtv

--- metaqtv/qtvlist.php
+++ metaqtv/qtvlist.php
@@ -5,6 +5,7 @@
 	$qtvlist = array(
 		"http://qw.suomicom.fi:28000/" => "Suomicom",
 		"http://butt.se:30000/" => "Butt.se",
+		"http://qtv.quakeservers.net:30000/" => "QuakeServers.net",
 		"http://147.231.20.214:30000/" => "Lamicka",
 	);
 	
