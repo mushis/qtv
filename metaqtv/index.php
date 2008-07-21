e39ded40a126713d6de26a9b52f32648e2c67b91
Jan Raszyk <jan.raszyk@gmail.com>
2008-07-21 11:34:33 +0000

    send utf-8 in http header too

--- metaqtv/index.php
+++ metaqtv/index.php
@@ -6,6 +6,7 @@
 	header("Cache-Control: no-store, no-cache, must-revalidate");
 	header("Cache-Control: post-check=0, pre-check=0", false);
 	header("Pragma: no-cache");
+	header("Content-type: text/html; charset=utf-8");
 
 
 	require "config.php";
