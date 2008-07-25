f9f6cdee6aace3028d7ed37e5756b941b12720ed
Alexandre Nizoux <alexandre@nizoux.fr>
2008-07-25 14:23:09 +0000

    Updated QW-DEV.net URL

--- metaqtv/qtvlist.php
+++ metaqtv/qtvlist.php
@@ -6,7 +6,7 @@
 		"http://qw.suomicom.fi:28000/" => "Suomicom",
 		"http://butt.se:30000/" => "Butt.se",
 		"http://qtv.quakeservers.net:30000/" => "QuakeServers.net",
-		"http://qw-dev.net:28000/" => "QW-DEV.net",
+		"http://qtv.qw-dev.net/" => "QW-DEV.net",
 		"http://147.231.20.214:30000/" => "Lamicka",
 	);
 	
