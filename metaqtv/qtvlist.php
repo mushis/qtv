1efb196a9bf0783da0c324af8925e0ba1b664ccb
Jan Raszyk <jan.raszyk@gmail.com>
2008-10-26 17:20:49 +0000

    adding phil's QTV

--- metaqtv/qtvlist.php
+++ metaqtv/qtvlist.php
@@ -8,6 +8,7 @@
 		"http://qtv.quakeservers.net:30000/" => "QuakeServers.net",
 		"http://qw-dev.net:28000/" => "QW-DEV.net",
 		"http://147.231.20.214:30000/" => "Lamicka",
+		"http://69.31.82.226:30000/" => "QuakePhil.com",
 	);
 	
 ?>
