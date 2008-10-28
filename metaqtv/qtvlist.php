41e33ad9408159ece481c6e585de6c747bc2c8a6
renzo <renzo@quakeworld.fi>
2008-10-28 13:55:57 +0000

    I own Quakeworld.fi.

--- metaqtv/qtvlist.php
+++ metaqtv/qtvlist.php
@@ -3,7 +3,7 @@
 	/// \file qtvlist.php
 	
 	$qtvlist = array(
-		"http://qw.suomicom.fi:28000/" => "Suomicom",
+		"http://quakeworld.fi:28000/" => "Quakeworld.fi",
 		"http://butt.se:30000/" => "Butt.se",
 		"http://qtv.quakeservers.net:30000/" => "QuakeServers.net",
 		"http://qw-dev.net:28000/" => "QW-DEV.net",
