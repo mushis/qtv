0b401ea1d5f62793e191e2eb5c7ac722d8eba201
Jan Raszyk <jan.raszyk@gmail.com>
2009-03-23 14:12:25 +0000

    updated to current list used on quakeworld.tv

--- metaqtv/qtvlist.php
+++ metaqtv/qtvlist.php
@@ -6,9 +6,14 @@
 		"http://quakeworld.fi:28000/" => "Quakeworld.fi",
 		"http://butt.se:30000/" => "Butt.se",
 		"http://qtv.quakeservers.net:30000/" => "QuakeServers.net",
-		"http://qw-dev.net:28000/" => "QW-DEV.net",
+		"http://qtv.qw-dev.net:28000/" => "QW-DEV.net",
 		"http://147.231.20.214:30000/" => "Lamicka",
-		"http://69.31.82.226:30000/" => "QuakePhil.com",
+		"http://conus.mine.nu:28000/" => "conus.mine.nu",
+		"http://qw.thejews.se:28000/" => "The Jews",
+		"http://qw.quakephil.com:29000/" => "QuakePhil.com",
+		"http://va.telefrag.me:30000/" => "VA.telefrag.ME",
+		"http://tx.telefrag.me:30000/" => "TX.telefrag.ME",
+		"http://wa.telefrag.me:30000/" => "WA.telefrag.ME",
 	);
 	
 ?>
